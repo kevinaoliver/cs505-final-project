9798b17752e8bdd3589dbca27298a05fd712bce4
ScarHam <78630879+ScarHam@users.noreply.github.com>
2022-04-19 20:01:03 -0400

    Create API.java

---
+++ httpcontrollers/API.java
@@ -0,0 +1,83 @@
+package cs505-final-project.httpcontrollers;
+
+import com.google.gson.Gson;
+import cs505-final-project.Launcher;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+@Path("/api")
+public class API {
+
+    @Inject
+    private javax.inject.Provider<org.glassfish.grizzly.http.server.Request> request;
+
+    private Gson gson;
+
+    public API() {
+        gson = new Gson();
+    }
+
+//MF 1 - Get team request
+    @GET
+    @Path("/getteam")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getteam() {
+        String responseString = "{}";
+        try {
+            System.out.println("WHAT");
+            Map<String,String> responseMap = new HashMap<>();
+            responseMap.put("team_name", "Hammer-Isa-Oliver");
+            responseMap.put("Team_members_sids", "[10730209,912349421,12257265]");
+            responseMap.put("app_status_code","0");
+
+            responseString = gson.toJson(responseMap);
+
+
+        } catch (Exception ex) {
+
+            StringWriter sw = new StringWriter();
+            ex.printStackTrace(new PrintWriter(sw));
+            String exceptionAsString = sw.toString();
+            ex.printStackTrace();
+
+            return Response.status(500).entity(exceptionAsString).build();
+        }
+        return Response.ok(responseString).header("Access-Control-Allow-Origin", "*").build();
+    }
+
+    @GET
+    @Path("/getlastcep")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getAccessCount(@HeaderParam("X-Auth-API-Key") String authKey) {
+        String responseString = "{}";
+        try {
+
+            //generate a response
+            Map<String,String> responseMap = new HashMap<>();
+            responseMap.put("lastoutput",Launcher.lastCEPOutput);
+            responseString = gson.toJson(responseMap);
+
+        } catch (Exception ex) {
+
+            StringWriter sw = new StringWriter();
+            ex.printStackTrace(new PrintWriter(sw));
+            String exceptionAsString = sw.toString();
+            ex.printStackTrace();
+
+            return Response.status(500).entity(exceptionAsString).build();
+        }
+        return Response.ok(responseString).header("Access-Control-Allow-Origin", "*").build();
+    }
+
+
+}
