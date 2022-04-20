a8b4ab6bd0ca220fe3eebef5953628a8af30ae9e
ScarHam <78630879+ScarHam@users.noreply.github.com>
2022-04-19 20:10:23 -0400

    Create run.sh
    Have been using "bash run.sh" command to run everything in my vm

---
+++ run.sh
@@ -0,0 +1,3 @@
+mvn clean package
+cd target
+java -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=9999 -Dcom.sun.management.jmxremote.rmi.port=9999 -Dcom.sun.management.jmxremote.local.only=false -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -jar cs505-final-template-1.0-SNAPSHOT.jar
